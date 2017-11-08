56e76f794464596da4b8b96d69119f18fd5f39c7
olegchursin <olegchursin@gmail.com>
2017-11-08 16:16:52 -0500

    edit comments

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-var button = document.getElementById('clicker'),
-    counter = document.getElementById('counter'),
-    katzDeliLine = [],
-    n = 0;
-window.onload = function() {
+var button = document.getElementById('clicker'), // fetch a button
+    counter = document.getElementById('counter'), // define a counter
+    katzDeliLine = [], // create empty array
+    n = 0; // starting number 
+window.onload = function() { // display 0 on page load
     counter.innerHTML = n;
 };
-button.onclick = function takeNumber(katzDeliLine) {
-    ++n
-    counter.innerHTML = `${n}`
-    katzDeliLine.push(n);
+button.onclick = function takeNumber(katzDeliLine) { // execute takeNumber() when button is clicked
+    ++n // increment n
+    counter.innerHTML = `${n}` // display new number after each click
+    katzDeliLine.push(n); // add new element to katzDeliLine array
 };
